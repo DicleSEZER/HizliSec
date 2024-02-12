b54cb45953e4bb7146a6f4ccb04db038e66738e4
ebarut7 <emrebarut1997@gmail.com>
2024-02-12 22:31:59 +0300

    Dtolar oluşturuldu

---
+++ HizliSec.Entities/Concrete/Dtos/UserDtos/CustomerDto.cs
@@ -0,0 +1,9 @@
+﻿using HizliSec.Core.Entities;
+using HizliSec.Entities.Abstract;
+
+namespace HizliSec.Entities.Concrete.Dtos.UserDtos
+{
+    public class CustomerDto : UserDto,IDto
+    {
+    }
+}
