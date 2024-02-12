92b12bbf846671321e54312eb5853c94222112be
ebarut7 <emrebarut1997@gmail.com>
2024-02-12 23:04:48 +0300

    Business katmanı açıldı

---
+++ HizliSec.Business/Abstract/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using HizliSec.Entities.Concrete;
+using HizliSec.Entities.Concrete.Dtos.UserDtos;
+using Microsoft.AspNetCore.Identity;
+
+namespace HizliSec.Business.Abstract
+{
+    public interface IAuthService
+    {
+        Task<List<string>> GetRolesAsync(AppUser user);
+        Task<IdentityResult> RemoveUserAsync(string userName);
+        Task<AppUser> GetUserAsync(string userName);
+        Task<IdentityResult> PasswordResetAsync(string userName, string newPassword);
+        Task<IdentityResult> UpdatePasswordAsync(string userName, string currentPassword, string newPassword);
+        Task<SignInResult> LoginAsync(LoginDto loginDto);
+        Task<IdentityResult> SellerRegisterAsync(SellerRegisterDto sellerRegisterDto);
+        Task<IdentityResult> CustomerRegisterAsync(CustomerRegisterDto customerRegisterDto);
+        Task SignOutAsync();
+        Task<IdentityResult> AddToRoleAsync(AppUser appUser, string role);
+    }
+}
