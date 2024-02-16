2e581c47d9ade02987282388b0c247eabb7e6098
Dicle SEZER <diclesezer3@outlook.com>
2024-02-16 21:03:30 +0300

    AuthController , authLogin.cshtml ve Microsoft.AspNetCore.Identity paketi eklendi.

---
+++ HizliSecc.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using HizliSec.Business.Abstract;
+using HizliSec.Entities.Concrete.Dtos.UserDtos;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HizliSecc.WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authservice;
+        public AuthController(IAuthService authservice)
+        {
+            _authservice = authservice;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task< IActionResult> Login(LoginDto loginDto)
+        {
+           Microsoft.AspNetCore.Identity.SignInResult response = await _authservice.LoginAsync(loginDto);
+            return View();
+        }
+    }
+}
