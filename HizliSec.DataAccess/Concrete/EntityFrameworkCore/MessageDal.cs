e64a1622cc0bb00f3a382e25798d25f8d4a30bab
ebarut7 <emrebarut1997@gmail.com>
2024-02-17 01:43:05 +0300

    #1 DataAccess kısmı düzenlendi

---
+++ HizliSec.DataAccess/Concrete/EntityFrameworkCore/MessageDal.cs
@@ -0,0 +1,15 @@
+﻿using HizliSec.Core.DataAccess.EntityFrameworkCore;
+using HizliSec.DataAccess.Abstract;
+using HizliSec.DataAccess.Concrete.EntityFrameworkCore.Context;
+using HizliSec.Entities.Concrete;
+
+namespace HizliSec.DataAccess.Concrete.EntityFrameworkCore
+{
+    public class MessageDal : RepositoryBase<Message>, IMessageDal
+    {
+        public MessageDal(HizliSecContext context) : base(context)
+        {
+
+        }
+    }
+}
